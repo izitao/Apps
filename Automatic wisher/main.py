109a4f2147c1b1a7a4fd4ef8c69e3c24cfca6486
izitao <79163107+izitao@users.noreply.github.com>
2021-12-06 19:17:28 +0100

    Create main.py

---
+++ Automatic wisher/main.py
@@ -0,0 +1,49 @@
+import pandas
+import datetime as dt
+import random
+import smtplib
+
+
+def letter_picker(name, sender, email_adress):
+    '''Creates a letter'''
+
+    letter_files = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+    letter_file = random.choice(letter_files)
+
+    with open(letter_file) as lf:
+        wish = lf.readlines()
+
+    letter = "".join(str(letter_in_wish) for letter_in_wish in wish)
+
+    letter = letter.replace("[NAME]", str(name))
+    letter = letter.replace("[SENDER]", str(sender))
+
+    send_letter(letter, email_adress)
+
+
+def send_letter(letter, email_adress):
+    '''Sends a letter to the specific person'''
+
+    my_email = "ilovecreatingapps@gmail.com"
+    password = "Apps123@"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=email_adress,
+                            msg=f"Subject:Happy birthday!\n\n{letter}"
+                            )
+
+
+#INPUTS
+
+sender = "Izitao"
+persons = pandas.read_csv("birthdays.csv")
+
+now = dt.datetime.today()
+
+for index, row in persons.iterrows():
+    if row.month == now.month:
+        if row.day == now.day:
+            letter_picker(row['name'], sender, row['email'])
