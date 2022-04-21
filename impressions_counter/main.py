e515521ff4bef5f79963db5c2a7f15dfb75baf22
izitao <79163107+izitao@users.noreply.github.com>
2022-04-21 15:38:11 +0200

    Update main.py

--- impressions_counter/main.py
+++ impressions_counter/main.py
@@ -1,112 +1,121 @@
 import pandas
 from datetime import datetime
 
-import pandas as pd
-from dateutil import parser
-
+#vstupni data
 filepath_clicks_raw = "raw_data/clicks.csv"
 filepath_impressions_raw = "raw_data/impressions.csv"
 
-filepath_clicks_edited = "raw_data/clicks_filter.csv"
+#vystupni data
+filepath_clicks_edited = "data/clicks_filter.csv"
 filepath_impressions_edited = "data/impressions_filter.csv"
 
+#inicializace vstupnich dat
 impressions_data_raw = pandas.read_csv(filepath_impressions_raw)
 clicks_data_raw = pandas.read_csv(filepath_clicks_raw)
-
-#print(impressions_data)
-#impressions_dict = impressions_data_raw.to_dict()
-#print(impressions_data["adId"])
-'''for row in impressions_dict:
-    print(row)'''
-#print(impressions_dict)
 
 #pracovni databaze
 impressions_data_edit = pandas.read_csv(filepath_impressions_raw)
 clicks_data_edit = pandas.read_csv(filepath_clicks_raw)
 
+#kriterium pozadovaneho casu
+min_delta = 10*60
 
-###impressions_data_raw = (impressions_data_raw.sort_values(by=["adId", "visitorHash"]))
-#print(impressions_data_raw)
+#id duplicitnich impresi
+impressions_ids_to_delete = []
 
 
-#for row in impressions_data_raw["impressionTime"]:
-    #print(row)
+def separate_tables(impressions_data_raw):
+    """ Rozdeli vstupni data do jednotlivych tabulek podle id reklamy(adId) """
+    # promenna obsahujici vypis vsech id reklam (adID)
+    adIds = []
 
-'''for rows in impressions_data_raw:
-    #if impressions_data_raw["adId"] == 1:
-    print(impressions_data_raw["adId"])
-    #print(rows)
-'''
+    # vyber vstupnich dat podle klice adId (bez opakovani)
+    for adId in impressions_data_raw["adId"]:
+        if adId not in adIds:
+            adIds.append(adId)
 
-#Make a list of adIds → vytvoreni samostatnych tabulek ze zdroje impressions (rozdeleno podle adID)
-adIds = []
-for adId in impressions_data_raw["adId"]: #vyber dat poole klice adId
-    if adId not in adIds:               #pridani neopakujicich se adId
-        adIds.append(adId)
-    #Sort increasingly by number
-for adId in range(len(adIds) - 1): #algoritmus pro vzestupne vytrideni
-    if adIds[adId - 1] < adIds[adId]:
-        temp = adIds[adId + 1]
-        adIds[adId + 1] = adIds[adId]
-        adIds[adId] = temp
+    # vzestupne setrideni promenne
+    adIds.sort()
 
-#Deleting duplicate rows based on condition < 10 MINs
-    #vytvoreni samostatnych tabulek dle adID
-impressions_data = []
-for adId in adIds:
-    #print(impressions_data_raw.loc[lambda df: df['adId'] == adId, :])
-    impressions_data.append(impressions_data_raw.loc[lambda df: df['adId'] == adId, :]) #pridani podtabulky ktere maji spolecne adId
-    #impressions_data_raw.loc[lambda df: df['adId'] == adId, :].drop_duplicates(subset=["visitorHash","impressionTime"], keep="first")
+    # vytvoreni samostatnych tabulek se spolecnym adID
+    impressions_data = []
+    for adId in adIds:
+        impressions_data.append(impressions_data_raw.loc[lambda df: df['adId'] == adId, :])
 
-    #serazeni kazde tabulky dle visitorHASH
-for table in impressions_data:
-    visitorHashes = []
-    for vh in table["visitorHash"]: #ziskani seznamu jednotlivych visitorashu v dane tabulce
-        #print(table[table.visitorHash == vh])
-        if vh not in visitorHashes:
-            visitorHashes.append(vh)
-    for vh in visitorHashes: #tabulka s konkretnim visitorhashem v dane tabulce
+    return impressions_data
+
+
+def order_by_visitor_hash(impressions_data):
+    """ Serazeni kazde tabulky podle visitorHash """
+
+    # seznam s id impresi k vymazani
+
+    for table in impressions_data:
+        visitor_hashes = []
+
+        # ziskani seznamu jednotlivych visitorhashu v dane tabulce (bez opakovani)
+        for vh in table["visitorHash"]:
+            if vh not in visitor_hashes:
+                visitor_hashes.append(vh)
+        get_visitor_timestamps(visitor_hashes, table)
+
+
+def get_visitor_timestamps(visitor_hashes, table):
+    """ Vytvori slovnik s timestampy daneho visitora k prislusne tabulce reklamy (adId) """
+
+    # vytvoreni slovniku ke kazdemu visitorovi | tabulka s konkretnim visitorhashem v dane tabulce
+    for vh in visitor_hashes:
         timestamp_dict = {}
         k = 0
-        # naskladani hodnot do slovniku
-        for index, row in table[table.visitorHash == vh].iterrows(): #vsechny navstevy daneho uzivatele #timestamp_dict = {poradi prvku ve slovniku: (index_v_tabulce, time_stamp)}
-            #print(type(datetime.fromisoformat(row["impressionTime"])))
-            #timestamp_dict[index] = datetime.fromisoformat(row["impressionTime"]) #prevedni ze stringu na datetime
-            timestamp_dict[k] = (index, datetime.fromisoformat(row["impressionTime"]))  #prevedni ze stringu na datetime
+
+        # vlozeni vsech navstev daneho visitora konkretniho adId do slovniku timestamp_dict = {poradi prvku ve slovniku: (impressionId, time_stamp)}
+        for index, row in table[table.visitorHash == vh].iterrows():
+            impression_id = row["impressionId"]
+            impression_time = datetime.fromisoformat(row["impressionTime"])
+            timestamp_dict[k] = (impression_id, impression_time)
             k += 1
 
-        #print(timpestamp_dict)
-        values_list = timestamp_dict.keys() #vytvoreni seznamu s klicema slovniku timestamp → slouzici pro dalsi praci
-        #print(timestamp_dict[0][1])
-        min_delta = 10*60
-
-        for value in range(len(values_list)-1):
-            #print(timestamp_dict[value+1][1] - timestamp_dict[value][1])
-            delta = (timestamp_dict[value+1][1] - timestamp_dict[value][1]) #volba casoveho udaje daneho radku v tabulce a vypocet rozdilu casu navstevy
-            delta = delta.total_seconds() #prevod na sekundy
-            if delta <= min_delta: #hlavni podminky
-                print(index)
-                impressionId_delete = (impressions_data_raw[impressions_data_raw.impressionId == index]["impressionId"].values[0]) #vyheldani impressionID z vychozich dat prevedeni na int → vstup pro identifikaci pro vymazani z tabulky clicks
-                impressions_data_edit.drop(index, axis=0, inplace=True) #vymazani duplicitniho radku v pracovni tabulce impressions
-                #clicks_data_edit.drop(columns=impressionId_delete)
-                click_index_delete = (clicks_data_edit[clicks_data_edit.impressionId == impressionId_delete].index.values) #ziskani indexu radku pro tabulku clicks ktery se vyhodnotli jako duplicitni v tabulce impressions
-                print(click_index_delete)
-                clicks_data_edit.drop(click_index_delete, axis=0, inplace=True) #vymazani duplicitniho radku v pracovni tabulce clicks
-                #print(table)
+        # vytvoreni seznamu s klici slovniku timestamp → slouzici pro dalsi praci
+        values_list = timestamp_dict.keys()
+        get_duplicate_impression_id(values_list, timestamp_dict)
 
 
+def get_duplicate_impression_id(values_list, timestamp_dict):
+    """ Na zaklade casoveho kriteria vyhodnoti duplicitni navstevy daneho visitora """
+
+    for value in range(len(values_list)-1):
+        if len(values_list) <= 1:
+            break
+        else:
+            # vypocet rozdilu casu dvou po sobe jdoucich navstev
+            delta = abs((timestamp_dict[value+1][1] - timestamp_dict[value][1]))
+            delta = delta.total_seconds()
+            # hlavni podminky casoveho kriteria
+            if delta <= min_delta:
+                print(timestamp_dict[value][0])
+                impression_id_delete = timestamp_dict[value][0]
+                impressions_ids_to_delete.append(impression_id_delete)
 
 
-print(clicks_data_raw)
-    #print(visitorHashes)
-print(impressions_data_edit)
-print(clicks_data_raw)
-print(clicks_data_edit)
+def delete_duplicate_visits(impression_ids):
+    """ Vymaze z dataframu duplicitni imprese """
+
+    impression_ids.sort(reverse=True)
+
+    for impression_id in impression_ids:
+        print(impression_ids)
+        impressions_data_edit.drop(index=impression_id-1, axis=0, inplace=True)
+        # ziskani indexu radku pro tabulku clicks ktery se vyhodnotli jako duplicitni v tabulce impressions
+        click_index_delete = (clicks_data_edit[clicks_data_edit.impressionId == impression_id].index.values)
+        clicks_data_edit.drop(click_index_delete, axis=0, inplace=True) #vymazani duplicitniho radku v pracovni tabulce clicks"""
+
+
+order_by_visitor_hash(separate_tables(impressions_data_raw))
+delete_duplicate_visits(impressions_ids_to_delete)
 
 #EXPORRT DO SOUBORU BEZ INDEXOVANI RADKU
+
 impressions_data_edit.to_csv(filepath_impressions_edited, index=False)
 clicks_data_edit.to_csv(filepath_clicks_edited, index=False)
-
-
-
+print(impressions_ids_to_delete)
 
